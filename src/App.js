408ba75978d926c59706418105986e72fb1df47b
Guddu Kumar <coldmilkhottea@gmail.com>
2023-11-01 00:21:36 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <h1>Hello </h1> */}
+        <Routes>
+          <Route path="personal-website/" element={<Home />} />
+          <Route path="personal-website/about" element={<About />} />
+          <Route path="personal-website/contact" element={<Contact />} />
+        </Routes>
     </div>
+    </>
   );
 }
 
